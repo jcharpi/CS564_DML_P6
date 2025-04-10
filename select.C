f4f1f26df56834ed688f737de98c82fa7ddec5c7
Josh Charpentier <charpentier.josh@gmail.com>
2025-04-09 19:59:39 -0500

    Final pass through on select.C - bug fixing

--- select.C
+++ select.C
@@ -1,7 +1,3 @@
-// select.C
-//
-// This file implements the selection operator for Minirel.
-//
 // A selection is implemented using a filtered HeapFileScan. The result of the selection is stored in the result relation called
 // result (a heapfile with this name will be created by the parser before QU_Select() is called). The project list is defined by the parameters projCnt
 // and projNames. Projection should be performed on the fly as each result tuple is being appended to the result table.
@@ -13,8 +9,11 @@
 //
 // This implementation sets up a filtered scan for a selection predicate and
 // performs on‐the‐fly projection via a helper function, ScanSelect().
+
 #include "catalog.h"
 #include "query.h"
+#include "stdio.h"
+#include "stdlib.h"
 
 const Status ScanSelect(const string &result,
                         const int projCnt,
@@ -28,17 +27,9 @@ const Status ScanSelect(const string &result,
  * Selects records from the specified relation.
  *
  * Returns:
- * 	OK on success
- * 	an error code otherwise
+ *   OK on success
+ *   an error code otherwise
  */
-
-// return rows that correspond to given filter
-// result: name of relation to store output in
-// projCnt: number of attributes to project
-// projNames: array of attrInfo structs describing attributes to project
-// attr: attribute used for filtering (if NULL, perform an unconditional scan)
-// op: operator defining the filtering condition
-// attrValue: search value supplied as character string
 const Status QU_Select(const string &result,
                        const int projCnt,
                        const attrInfo projNames[],
@@ -46,16 +37,15 @@ const Status QU_Select(const string &result,
                        const Operator op,
                        const char *attrValue)
 {
-  // Qu_Select sets up things and then calls ScanSelect to do the actual work
+  // QU_Select sets up things and then calls ScanSelect to do the work.
   cout << "Doing QU_Select " << endl;
-  Status status;
+  Status status = OK;
+  AttrDesc projDesc[projCnt];          // Each element corresponds to an attribute in the output (the projection list)
+  AttrDesc *attrDesc = new AttrDesc(); // Will store the detailed attribute descriptor for filtering, if needed.
+  int reclen = 0;                      // Total output record length in bytes.
+  const char *filter;
 
-  // Create an array of AttrDesc structures, one for each attribute in the projection list.
-  // The projection list (projNames) is provided as an array of attrInfo structures that
-  // have basic attribute details (such as relation and attribute names). Here we convert
-  // these into complete attribute descriptions by retrieving additional information (offset,
-  // type, length) from the catalog.
-  AttrDesc projDesc[projCnt]; // each element corresponds to an attribute in the output
+  // Convert basic attribute information (from projNames) into full attribute descriptors (projDesc) by retrieving metadata from the catalog.
   for (int i = 0; i < projCnt; i++)
   {
     status = attrCat->getInfo(projNames[i].relName,
@@ -63,41 +53,55 @@ const Status QU_Select(const string &result,
                               projDesc[i]);
     if (status != OK)
       return status;
+    reclen += projDesc[i].attrLen;
   }
 
-  // If a filtering condition is specified (indicated by attrValue being non-NULL),
-  // then we need to retrieve the detailed attribute description for the filtering attribute.
-  // This descriptor (attrDesc) contains metadata such as the offset within a record, type,
-  // and length, all of which are required for performing the filtered scan later.
-  AttrDesc attrDesc; // corresponds to a single element used for filtering
-  if (attr != NULL)
-  {
-    status = attrCat->getInfo(attr->relName, attr->attrName, attrDesc);
-    if (status != OK)
-      return status;
-  }
-
-  // Calculate the length of the output record by summing the lengths of the projected attributes.
-  int outputRecordLength = 0; // the total number of bytes required for each output tuple.
-  for (int i = 0; i < projCnt; i++)
-  {
-    outputRecordLength += projDesc[i].attrLen;
-  }
-
-  // If no filtering attribute is specified, perform an unconditional scan. i.e. no WHERE clause provided by the user and we should process every tuple.
+  // If no filtering attribute is specified (i.e., no WHERE clause), perform an unconditional scan.
   if (attr == NULL)
   {
-    return ScanSelect(result, projCnt, projDesc, &projDesc[0], EQ, NULL, outputRecordLength);
+    attrDesc = NULL;
+    status = ScanSelect(result, projCnt, projDesc, attrDesc, op, NULL, reclen);
+    if (status != OK)
+    {
+      return status;
+    }
   }
-  // If a filtering attribute is provided, then call ScanSelect with the detailed filtering information obtained in attrDesc,
-  // along with the appropriate operator (op) and the provided filter value (attrValue).
-  return ScanSelect(result, projCnt, projDesc, &attrDesc, op, attrValue, outputRecordLength);
+  else
+  {
+    int intConversion;
+    float floatConversion;
+    // Convert the filter string to the proper type based on the attribute's type.
+    switch (attr->attrType)
+    {
+    case INTEGER:
+      intConversion = atoi(attrValue);
+      filter = (char *)&intConversion;
+      break;
+    case FLOAT:
+      floatConversion = atof(attrValue);
+      filter = (char *)&floatConversion;
+      break;
+    case STRING:
+      filter = attrValue;
+      break;
+    }
+    // Retrieve the complete attribute descriptor for the filtering attribute.
+    status = attrCat->getInfo(attr->relName, attr->attrName, *attrDesc);
+    if (status != OK)
+    {
+      return status;
+    }
+    status = ScanSelect(result, projCnt, projDesc, attrDesc, op, filter, reclen);
+    if (status != OK)
+    {
+      return status;
+    }
+  }
+  return OK;
 }
 
 // This function sets up a filtered scan on the input table and performs on-the-fly projection into the output relation.
 const Status ScanSelect(const string &result,
-#include "stdio.h"
-#include "stdlib.h"
                         const int projCnt,
                         const AttrDesc projNames[],
                         const AttrDesc *attrDesc,
@@ -106,86 +110,77 @@ const Status ScanSelect(const string &result,
                         const int reclen)
 {
   cout << "Doing HeapFileScan Selection using ScanSelect()" << endl;
+  
   Status status;
-  int outputTupleCount = 0; // number of tuples inserted into the output relation
+  string relName = projNames[0].relName;
+  HeapFileScan *heapScan = new HeapFileScan(relName, status);
 
-  // Result relation is a heapfile which has already been created by the parser.
-  // We use InsertFileScan to insert the projected tuples into this result relation.
-  InsertFileScan resultRel(result, status);
-  if (status != OK)
-    return status;
-
-  // The table to scan is identified by the relName field in the attribute descriptor.
-  // HeapFileScan will allow us to iterate over tuples in the table.
-  HeapFileScan scan(string(attrDesc->relName), status);
-  if (status != OK)
-    return status;
-
-  // Start the scan on the input table:
-  // 1. If filter is NULL, perform an unconditional scan.
-  if (filter == NULL)
+  if (attrDesc != NULL)
   {
-    status = scan.startScan(attrDesc->attrOffset, attrDesc->attrLen, STRING, NULL, EQ);
-  }
-
-  // 2. If filter is provided, perform a filtered scan using the supplied value.
-  // Determine attribute's data type and convert the filter string accordingly.
-  else
-  {
-    Datatype type;
-    void *updatedTypeFilter;
-    if (attrDesc->attrType == STRING)
+    // Start a filtered scan using the provided filter value.
+    if (attrDesc->attrType == INTEGER)
     {
-      type = STRING;
-      updatedTypeFilter = (void *)filter; // No conversion needed for strings
+      heapScan->startScan(attrDesc->attrOffset, attrDesc->attrLen, INTEGER, filter, op);
     }
-    else if (attrDesc->attrType == INTEGER)
+    else if (attrDesc->attrType == FLOAT)
     {
-      type = INTEGER;
-      int tempInt = atoi(filter); // Convert the filter to an integer
-      updatedTypeFilter = &tempInt;
+      heapScan->startScan(attrDesc->attrOffset, attrDesc->attrLen, FLOAT, filter, op);
     }
     else
     {
-      type = FLOAT;
-      float tempFloat = atof(filter); // Convert the filter to an float
-      updatedTypeFilter = &tempFloat;
+      heapScan->startScan(attrDesc->attrOffset, attrDesc->attrLen, STRING, filter, op);
     }
-    status = scan.startScan(attrDesc->attrOffset, attrDesc->attrLen, type, (char *)updatedTypeFilter, op);
-    if (status != OK)
-      return status;
+  }
+  else
+  {
+    // Unconditional scan
+    heapScan->startScan(0, 0, STRING, NULL, EQ);
+  }
+  if (status != OK)
+  {
+    return status;
   }
 
-  // Prep a temporary buffer to build each output tuple. The output tuple will consist of only the projected attributes and must
-  // be exactly reclen bytes long.
-  char outputData[reclen];
-  Record outputRec;
-  outputRec.data = (void *)outputData;
-  outputRec.length = reclen;
-
-  // Process each tuple returned by the scan:
-  // For each matching tuple, copy out each projected attribute (using its offset and length)
-  // into the output buffer, and insert the newly formed tuple into the result relation.
-  RID rid;
-  while (scan.scanNext(rid) == OK)
+  // Open the result table where the output tuples will be inserted.
+  InsertFileScan outTable(result, status);
+  if (status != OK)
   {
+    return status;
+  }
+
+  // Process each tuple returned by the scan.
+  RID rid;
+  while (heapScan->scanNext(rid) == OK)
+  {
+    // Retrieve the current tuple from the input table.
     Record rec;
-    status = scan.getRecord(rec);
+    status = heapScan->HeapFile::getRecord(rid, rec);
     ASSERT(status == OK);
 
-    int outputOffset = 0;
-    // For a match, copy data into output record
+    // Allocate a buffer for the output tuple.
+    char *data = new char[reclen];
+    Record tempRecord = {data, reclen};
+    int offset = 0;
+
+    // Copy each projected attribute's data from the input tuple to the output buffer.
     for (int i = 0; i < projCnt; i++)
     {
-      memcpy(outputData + outputOffset, (char *)rec.data + projNames[i].attrOffset, projNames[i].attrLen);
-      outputOffset += projNames[i].attrLen;
+      // point to the next free byte in the output buffer where the current attribute should be copied.
+      char *destPtr = data + offset;
+      // point to the location in the input record where the attribute's value begins.
+      char *srcPtr = ((char *)rec.data) + projNames[i].attrOffset;
+      int len = projNames[i].attrLen;
+      memcpy(destPtr, srcPtr, len);
+      offset += len;
     }
 
-    // Insert the constructed record into the result relation.
-    RID outRID;
-    status = resultRel.insertRecord(outputRec, outRID);
-    ASSERT(status == OK);
-    outputTupleCount++;
+    // Insert the newly constructed output tuple into the result relation.
+    RID tempRID;
+    status = outTable.insertRecord(tempRecord, tempRID);
+    if (status != OK)
+    {
+      return status;
+    }
   }
-  return OK;
+  return status;
 }
