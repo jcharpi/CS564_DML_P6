7d1b11d1c2729caf686cf1b894cb709c01f0b96d
Rahul Polavarapu <45112506+Rahul3130@users.noreply.github.com>
2025-04-20 12:54:24 -0500

    Update delete.C

--- delete.C
+++ delete.C
@@ -9,12 +9,59 @@
  * 	an error code otherwise
  */
 
-const Status QU_Delete(const string &relation,
-                       const string &attrName,
-                       const Operator op,
-                       const Datatype type,
-                       const char *attrValue)
+ const Status QU_Delete(const string & relation, 
+  const string & attrName, 
+  const Operator op,
+  const Datatype type, 
+  const char *attrValue)
 {
-  // part 6
-  return OK;
+Status status;
+HeapFileScan* hfs = new HeapFileScan(relation, status);
+if(status != OK)
+return status;
+
+AttrDesc attrDesc;
+RID rid;
+
+attrCat->getInfo(relation, attrName, attrDesc);
+
+int offset = attrDesc.attrOffset;
+int length = attrDesc.attrLen;
+
+int intValue;
+float floatValue;
+
+switch(type)
+{
+case STRING:
+status = hfs->startScan(offset, length, type, attrValue, op);
+break;
+
+case INTEGER:
+intValue = atoi(attrValue);
+status = hfs->startScan(offset, length, type, (char *)&intValue, op);
+break;
+
+case FLOAT:
+floatValue = atof(attrValue);
+status = hfs->startScan(offset, length, type, (char *)&floatValue, op);
+break;
+}
+
+if (status != OK)
+{
+delete hfs;
+return status;
+}
+
+while((status = hfs->scanNext(rid)) == OK) 
+{
+if ((status = hfs->deleteRecord()) != OK)
+return status;
+}
+
+hfs->endScan();
+delete hfs;
+
+return OK;
 }
